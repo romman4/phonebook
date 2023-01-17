b93cd4c7b41afda6950dd91b4136cbf6555960be
theromman4 <theromman4@gmail.com>
2023-01-17 15:04:54 +0300

    changed constructor to lombok RequiredArgsConstructor in PhonebookServiceFile

--- src/main/java/ru/netology/theromman/phonebook/service/PhonebookServiceFile.java
+++ src/main/java/ru/netology/theromman/phonebook/service/PhonebookServiceFile.java
@@ -1,5 +1,6 @@
 package ru.netology.theromman.phonebook.service;
 
+import lombok.RequiredArgsConstructor;
 import ru.netology.theromman.phonebook.model.Contact;
 import ru.netology.theromman.phonebook.repository.PhonebookRepository;
 
@@ -7,13 +8,10 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 
+@RequiredArgsConstructor
 public class PhonebookServiceFile implements PhonebookService {
 
     private final PhonebookRepository phonebookRepository;
-
-    public PhonebookServiceFile(PhonebookRepository phonebookRepository) {
-        this.phonebookRepository = phonebookRepository;
-    }
 
     @Override
     public void createContact(Contact contact) {
